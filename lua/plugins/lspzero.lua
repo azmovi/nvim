74ad6da83cc6da181a2cbe6edb9f75e3a758ff70
azevedo <azevedoantoniocicero@gmail.com>
2023-09-22 22:32:44 -0300

    indent lines baixado

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -1,87 +1,112 @@
-return
-{
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup, we are doing it manually
-      local map = require("helpers.keys").map
-      map('n', '<A-d>', ':lua vim.lsp.buf.hover()<CR>')
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    config = true,
-  },
-
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      {'L3MON4D3/LuaSnip'},
+return {
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        lazy = true,
+        config = false,
+        init = function()
+            local map = require("helpers.keys").map
+            map('i', '<A-d>', ':lua vim.lsp.buf.hover()<CR>')
+            map('n', '<A-d>', ':lua vim.lsp.buf.hover()<CR>')
+            vim.g.lsp_zero_extend_cmp = 0
+            vim.g.lsp_zero_extend_lspconfig = 0
+        end,
     },
-    config = function()
-      -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
-
-      -- And you can configure cmp even more, if you want to.
-      local cmp = require('cmp')
-
-      cmp.setup({
-        formatting = lsp_zero.cmp_format(),
-        mapping = cmp.mapping.preset.insert({
-            -- `Enter` key to confirm completion
-            ['<CR>'] = cmp.mapping.confirm({select = false}),
-        }),
-        preselect = 'item',
-        completion = {
-            completeopt = 'menu, menuone, noinsert',
-            --autocomplete 
-            --se tiver ativado, deixa comentado 
-            --caso contrário o lsp quebra  
-            autocomplete = false,
+    {
+        'williamboman/mason.nvim',
+        lazy = false,
+        config = true,
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'},
         },
-      })
-    end
-  },
-  -- LSP
-  {
-    'neovim/nvim-lspconfig',
-    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-    event = {'BufReadPre', 'BufNewFile'},
-    dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'williamboman/mason-lspconfig.nvim'},
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_cmp()
+
+            local cmp = require('cmp')
+            cmp.setup({
+                formatting = lsp_zero.cmp_format(),
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                }),
+                preselect = 'item',
+                completion = {
+                    completeopt = 'menu,menuone,noinsert',
+                    autocomplete = false,
+                },
+            })
+        end,
     },
-    config = function()
-      -- This is where all the LSP shenanigans will live
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_lspconfig()
+    {
+        'neovim/nvim-lspconfig',
+        cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+        event = {'BufReadPre', 'BufNewFile'},
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'williamboman/mason-lspconfig.nvim'},
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
 
-      lsp_zero.on_attach(function(_, bufnr)
-          -- see :help lsp-zero-keybindings
-          -- to learn the available actions
-          lsp_zero.default_keymaps({buffer = bufnr})
-      end)
+            lsp_zero.on_attach(function(_, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
+            end)
 
-      require('mason-lspconfig').setup({
-          ensure_installed = {},
-          handlers = {
-              lsp_zero.default_setup,
-              lua_ls = function()
-                  -- (Optional) Configure lua language server for neovim
-                  local lua_opts = lsp_zero.nvim_lua_ls()
-                  require('lspconfig').lua_ls.setup(lua_opts)
-              end,
-          },
-      })
-  end
-  }
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = {
+                    lsp_zero.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        require('lspconfig').lua_ls.setup(lua_opts)
+                    end,
+                },
+                make_pyright_config = function()
+                    local config = mason_lspconfig.make_config()
+                    config.settings = {
+                        python = {
+                            analysis = {
+                                autoImportCompletion = true,
+                                autoSearchPaths = true,
+                                diagnosticMode = "openFilesOnly",
+                                typeCheckingMode = "off",
+                                useLibraryCodeForTypes = true,
+                            },
+                        },
+                    }
+
+                    local function venv_path()
+                        if vim.fn.isdirectory(".venv") == 1 then
+                            return vim.fn.getcwd() .. "/.venv"
+                        end
+
+                        if vim.fn.filereadable("pyproject.toml") == 1 then
+                            local poetry = vim.fn.system("poetry env info -p"):gsub("\n", "")
+                            if vim.fn.isdirectory(poetry) then
+                                return poetry
+                            end
+                        end
+                    end
+
+                    local venv = venv_path()
+                    if venv == nil then
+                        return config
+                    end
+
+                    local packages = vim.fn.system(venv .. "/bin/python -m pip --list")
+                    if string.find(packages, "mypy") ~= nil then
+                        config.settings.python.analysis.typeCheckingMode = "basic"
+                    end
+
+                    config.settings.python.pythonPath = venv .. "/bin/python"
+                    return config
+                end
+            })
+        end,
+    }
 }
