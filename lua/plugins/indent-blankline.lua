8fa3eef717a0780b8631bef3e3a985a1f0a38ff0
azevedo <azevedoantoniocicero@gmail.com>
2023-10-01 21:47:34 -0300

    nao consegui arrumar esse lixo

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,5 +4,11 @@ return
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         opts = {},
+        config = function()
+            require("ibl").setup {
+               enabled = true,
+               show_start = false,
+            }
+        end
     }
 }
