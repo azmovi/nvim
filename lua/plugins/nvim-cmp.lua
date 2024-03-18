9a53a66e59db1093b0b6f2737627426b96345ef6
azevedo <azevedoantoniocicero@gmail.com>
2024-03-17 21:01:29 -0300

    tentando arrumar o cmp mas nao sei se funcionou

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -6,56 +6,59 @@ return {
     "hrsh7th/cmp-path", -- source for file system paths
     "L3MON4D3/LuaSnip", -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
   },
   config = function()
     local cmp = require("cmp")
 
     local luasnip = require("luasnip")
 
-    cmp.setup({
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-
-      window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-      },
-
-      mapping = cmp.mapping.preset.insert({
-          ["<C-X>"] = cmp.mapping.abort(), -- close completion window
-          ['<C-p>'] = cmp.mapping(function()
-              if cmp.visible() then
-                  cmp.select_prev_item({ behavior = 'insert' })
-              else
-                  cmp.complete()
-              end
-          end),
-          ['<C-n>'] = cmp.mapping(function()
-              if cmp.visible() then
-                  cmp.select_next_item({ behavior = 'insert' })
-              else
-                  cmp.complete()
-              end
-          end),
-          ["<CR>"] = cmp.mapping.confirm({ select = true}),
-      }),
-      -- sources for autocompletion
-      sources = cmp.config.sources({
+    local default_cmp_sources ={
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
-      }),
+      }
 
-      preselect = 'item',
-      completion = {
-        autocomplete = false,
-        completeopt = "menu, menuone, popup",
-      },
-    })
+      local mappings = {
+            ['<CR>'] = cmp.mapping.confirm({ select = false }),
+            ['<C-x>'] = cmp.mapping.abort(),
+            ['<C-p>'] = cmp.mapping(function()
+                if cmp.visible() then
+                    cmp.select_prev_item({ behavior = 'insert' })
+                else
+                    cmp.complete()
+                end
+            end),
+            ['<C-n>'] = cmp.mapping(function()
+                if cmp.visible() then
+                    cmp.select_next_item({ behavior = 'insert' })
+                else
+                    cmp.complete()
+                end
+            end),
+        }
+                local opts = {
+            completion = {
+                completeopt = 'menu,menuone',
+            },
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = {
+                    scrollbar = false,
+                    border = "rounded",
+                },
+                documentation = {
+                    border = "rounded",
+                },
+            },
+            sources = default_cmp_sources,
+            mapping = mappings,
+        }
+
+        cmp.setup(opts)
   end,
 }
