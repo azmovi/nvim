cf66398b7228e6ab31a14329f23f6e77f782ac06
azevedo <azevedoantoniocicero@gmail.com>
2024-06-27 13:18:18 -0300

    AAAAAAA

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -1,6 +1,4 @@
 return {
-	{
-		"jiangmiao/auto-pairs",
-	},
+    "jiangmiao/auto-pairs"
 }
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,3 @@
-return
-{
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "python", "markdown", "cpp", "java" },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end
+return {
+    'nvim-treesitter/nvim-treesitter'
 }
